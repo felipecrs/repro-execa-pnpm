c7230b250b49a2f04fed7133492dee647a3b1290
Felipe Santos <felipecassiors@gmail.com>
2025-07-14 22:16:59 -0300

    Potential solution

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import { execa } from 'execa';
+import { execa } from "execa";
 
 export async function main() {
-    await execa('open-cli', ["--version"], {
-        preferLocal: true,
-        stdio: 'inherit',
-    })
+  const localDir = import.meta.dirname;
+  await execa("open-cli", ["--version"], {
+    preferLocal: true,
+    localDir,
+    stdio: "inherit",
+  });
 }
 
 if (import.meta.url === `file://${process.argv[1]}`) {
-    await main();
+  await main();
 }
